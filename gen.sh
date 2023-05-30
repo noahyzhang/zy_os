f335bfe94c716240d8ad06118846a69eba09e453
zhangyi83 <zhangyi83@baidu.com>
2023-05-30 19:36:31 +0800

    use 8259A to implement interrupts

--- gen.sh
+++ gen.sh
@@ -11,10 +11,33 @@ fi
 if [ ! -d "./out/kernel" ];then 
     mkdir out/kernel 
 fi 
+
+echo -e "================================================"
+echo -e "生成 hd60M.img 文件"
 bximage -q -func="create" -hd=60 -imgmode="flat" -sectsize=512 hd60M.img
+
+echo -e "生成 mbr.bin 文件"
 nasm -I ./boot/include/ -o ./out/boot/mbr.bin ./boot/mbr.s && dd if=./out/boot/mbr.bin of=./hd60M.img bs=512 count=1  conv=notrunc
+
+echo -e "生成 loader.bin 文件"
 nasm -I ./boot/include/ -o ./out/boot/loader.bin ./boot/loader.s && dd if=./out/boot/loader.bin of=./hd60M.img bs=512 count=4 seek=2 conv=notrunc
+
+echo -e "生成 print.o 文件"
 nasm -f elf -o out/kernel/print.o lib/kernel/print.s
-gcc -m32 -I lib/kernel/ -c -o out/kernel/main.o kernel/main.c
-ld -melf_i386  -Ttext 0xc0001500 -e main -o ./out/kernel/kernel.bin out/kernel/main.o out/kernel/print.o && \
-    dd if=./out/kernel/kernel.bin of=./hd60M.img bs=512 count=200 seek=9 conv=notrunc
+
+echo -e "生成 kernel.o 文件"
+nasm -f elf -o out/kernel/kernel.o kernel/kernel.s
+
+echo -e "生成 interrupt.o 文件"
+gcc -m32 -I lib/kernel/ -I lib/ -I kernel/ -c -fno-builtin -fstack-protector -o out/interrupt.o kernel/interrupt.c
+
+echo -e "生成 init.o 文件"
+gcc -m32 -I lib/kernel/ -I lib/ -I kernel/ -c -fno-builtin -o out/init.o kernel/init.c
+
+echo -e "生成 main.o 文件"
+gcc -m32 -I lib/kernel/ -I lib/ -I kernel/ -c -fno-builtin -o out/kernel/main.o kernel/main.c
+
+echo -e "生成 kernel.bin 并且拷贝到硬盘"
+ld -melf_i386  -Ttext 0xc0001500 -e main -o ./out/kernel/kernel.bin \
+    out/kernel/main.o out/kernel/print.o out/init.o out/interrupt.o out/kernel/kernel.o \
+    &&  dd if=./out/kernel/kernel.bin of=./hd60M.img bs=512 count=200 seek=9 conv=notrunc 
--- kernel/main.c
+++ kernel/main.c
@@ -1,16 +1,26 @@
 #include "print.h"
+#include "init.h"
 
 void main(void) {
+    // 测试 print 函数
     put_str("I am kernel\n");
-    put_str("hello world 2023-05-20 11:52\n");
-    put_int(0);
-    put_char('\n');
-    put_int(9);
-    put_char('\n');
-    put_int(0x00021a3f);
-    put_char('\n');
-    put_int(0x12345678);
-    put_char('\n');
-    put_int(0x00000000);
+    // put_str("hello world 2023-05-20 11:52\n");
+    // put_int(0);
+    // put_char('\n');
+    // put_int(9);
+    // put_char('\n');
+    // put_int(0x00021a3f);
+    // put_char('\n');
+    // put_int(0x12345678);
+    // put_char('\n');
+    // put_int(0x00000000);
+
+    // 测试中断
+    init_all();
+    // 打开中断，使用 sti 指令，他会将标志寄存器 eflags 中的 IF 位置 1
+    // 这样来自中断代理 8259A 的中断信号便被处理器受理
+    // 这里主要为了验证时钟中断
+    asm volatile("sti");
+
     for (;;) {}
 }
