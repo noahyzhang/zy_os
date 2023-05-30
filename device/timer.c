1ddfac83578ef0a47e418dd1a9a32abe5958a0e9
zhangyi83 <zhangyi83@baidu.com>
2023-05-30 22:24:40 +0800

    modify compilation warning

--- device/timer.c
+++ device/timer.c
@@ -3,8 +3,8 @@
 #include "print.h"
 
 #define IRQ0_FREQUENCY	   1
-#define INPUT_FREQUENCY	   1193180
-#define COUNTER0_VALUE	   INPUT_FREQUENCY / IRQ0_FREQUENCY
+#define INPUT_FREQUENCY	   1193180U
+#define COUNTER0_VALUE	   ((uint16_t)INPUT_FREQUENCY / (uint16_t)IRQ0_FREQUENCY)
 #define CONTRER0_PORT	   0x40
 #define COUNTER0_NO	   0
 #define COUNTER_MODE	   2
