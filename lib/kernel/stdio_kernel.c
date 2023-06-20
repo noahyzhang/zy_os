4a458d40aacd8d4344ac6c378828ad30a79f23e2
zhangyi83 <zhangyi83@baidu.com>
2023-06-20 17:32:42 +0800

    add kernel print func

---
+++ lib/kernel/stdio_kernel.c
@@ -0,0 +1,17 @@
+#include "lib/kernel/stdio_kernel.h"
+#include "lib/kernel/print.h"
+#include "lib/stdio.h"
+#include "device/console.h"
+#include "kernel/global.h"
+
+#define va_start(args, first_fix) args = (va_list)&first_fix
+#define va_end(args) args = NULL
+
+void printk(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    char buf[1024] = {0};
+    vsprintf(buf, format, args);
+    va_end(args);
+    console_put_str(buf);
+}
---
+++ lib/kernel/stdio_kernel.h
@@ -0,0 +1,19 @@
+/**
+ * @file stdio_kernel.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-06-20
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef LIB_KERNEL_STDIO_KERNEL_H_
+#define LIB_KERNEL_STDIO_KERNEL_H_
+
+#include "lib/stdint.h"
+
+void printk(const char* format, ...);
+
+#endif  // LIB_KERNEL_STDIO_KERNEL_H_
